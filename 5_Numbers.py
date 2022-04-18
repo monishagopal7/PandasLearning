adcfb52803aa2a9bce1fdc88ac7636a0755f5dea
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-04-18 11:45:52 -0700

    Add files via upload

---
+++ 5_Numbers.py
@@ -0,0 +1,19 @@
+#import externl files
+from math import *
+from unittest.mock import seal
+
+print(3 + 4.5 )
+print(3 * (4 + 5))
+print(10 % 3)
+my_num= -5
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(pow(77, 2))
+print(max(4, 9))
+print(min(6, 9))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.7))
+print(seal(2.1))
+print(sqrt(36))
