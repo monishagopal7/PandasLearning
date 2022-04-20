987613f37504f8da1a3f4b0a3bab55af75d3b1a4
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-04-19 22:28:05 -0700

    Add files via upload

---
+++ 6_InputFromUser.py
@@ -0,0 +1,24 @@
+name = input("Enter your name : ")
+age = input("Enter your age: ")
+print("Hi " + name +"!" + "You are " + age + " years old ")
+print("")
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number ")
+result = num1 + num2
+print (result)
+print("")
+#when we get input from user by default pythin converts into string
+
+print("")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number ")
+result = int(num1) + int(num2)
+print (result)
+
+#for decimal
+print("")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number ")
+result = float(num1) + float(num2)
+print (result)
