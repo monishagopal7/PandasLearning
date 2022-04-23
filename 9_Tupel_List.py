f558bad8e903bfe3ed2150791dcd910fe5d9ef5e
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-04-23 08:56:29 -0700

    Add files via upload

---
+++ 9_Tupel_List.py
@@ -0,0 +1,15 @@
+coordinates = (4, 5)
+print(coordinates)
+#coordinates[1]=10
+print(coordinates[0])
+print(coordinates[1])
+coordinates = ((4, 5), (6,7), (8,9))
+
+#tupels are immutable , means you can't change or modify
+t1 = ('a',)
+print(type(t1))
+#coordinates[1]=10
+print(coordinates)
+print(coordinates[0])
+print(coordinates[1])
+
