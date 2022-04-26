4a1946c4948a09838614b5e4a4d4ed97438c47a8
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-04-26 10:52:39 -0700

    Add files via upload

---
+++ 10_Functions.py
@@ -0,0 +1,18 @@
+#Core concept for Python
+def say_hi():
+    print("Hello User")
+print("Top")
+say_hi()
+print("Bottom")
+print("hmm")
+
+#return statement
+def cube(num):
+    return num*num*num
+    print("Code") #This will never be printed
+print(cube(3))
+
+
+result=cube(5)
+print(result)
+
