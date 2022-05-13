5e2c99182dab10972750339d8c33643c5702b9f8
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-05-12 20:43:50 -0700

    Add files via upload

---
+++ 14_Dictionaries.py
@@ -0,0 +1,36 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Dec"))
+print(monthConversions.get("Luv"))
+
+monthConversions = {
+    0: "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+print(monthConversions[0])
+print(monthConversions.get("Dec"))
+print(monthConversions.get("Luv"))
