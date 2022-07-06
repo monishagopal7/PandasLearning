7eb7513f5821f628d75b0dfe17cae6ca3e2d1008
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-07-06 11:22:58 -0700

    Add files via upload

---
+++ 3-variables.py
@@ -0,0 +1,11 @@
+#use descriptive variable names
+print(f'hi {5 * 5 *5 } wassup')
+calculation_to_units = 24 * 60 * 60
+name_of_unit = "seconds"
+
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
